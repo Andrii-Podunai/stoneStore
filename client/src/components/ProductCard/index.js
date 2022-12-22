640efd95cbc1b56545abe1fb36859fbc5dd2666f
Віталій Талько <cotleopol1d@gmail.com>
2022-12-22 13:37:35 +0000

    Product card

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Card } from 'antd';
+import './main.scss';
 
 function ProductCard({ price, image, category, title }) {
   return (
     <Card
       hoverable
-      style={{
-        width: 240,
-        fontFamily: 'Roboto, sans-serif',
-        maxHeight: '456px',
-      }}
+      className="home-cards"
       bodyStyle={{ paddingLeft: '10px', paddingRight: '10px' }}
       cover={<img style={{ height: '300px', objectFit: 'cover' }} alt="product" src={image} />}>
       <h4 className="fs-6 mb-3">{title}</h4>
