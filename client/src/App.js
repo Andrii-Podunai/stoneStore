d08a2dcf80d43ad76fce86a1c86a322ef3f279af
Максим Подлесный <volkovmaks2100@gmail.com>
2022-12-25 17:13:27 +0000

    add login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,15 @@ import './App.scss';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading === true) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
       <Header />
