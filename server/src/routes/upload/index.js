8b13620a64e1c54dd7e6d16288ed9684f8ade956
Віталій Талько <cotleopol1d@gmail.com>
2023-01-09 18:32:38 +0000

    Delete image from server

--- server/src/routes/upload/index.js
+++ server/src/routes/upload/index.js
@@ -17,7 +17,7 @@ async function uploadHandler(request, reply) {
       const imageType = image.filename.match(/\.(jpg|png|webp|jpeg)/g)[0];
       const newFileName = nanoid() + imageType;
       const storedFile = fs.createWriteStream(
-        __dirname + '/public/' + '/cards-images/' + newFileName
+        __dirname + '/public/' + 'cards-images/' + newFileName
       );
       await pump(image.file, storedFile);
       files.push({ name: newFileName, originalName: image.filename });
@@ -28,11 +28,27 @@ async function uploadHandler(request, reply) {
   }
 }
 
+async function deleteHandler(request, reply) {
+  try {
+    request.body.forEach((name) => {
+      fs.unlinkSync(__dirname + '/public/' + 'cards-images/' + name);
+    });
+    reply.send();
+  } catch (error) {
+    reply.code(error.status || 500).send(error);
+  }
+}
+
 export default (fastify, __, done) => {
   fastify.route({
     method: 'POST',
     url: '/',
     handler: uploadHandler,
+  });
+  fastify.route({
+    method: 'DELETE',
+    url: '/',
+    handler: deleteHandler,
   });
   done();
 };
