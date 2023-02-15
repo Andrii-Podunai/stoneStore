a8f0bea874a834943ed31e68ebef212c89d08974
DimaBialik <byalik.dima@gmail.com>
2023-02-15 19:53:04 +0200

    add reducer

--- client/src/index.js
+++ client/src/index.js
@@ -5,18 +5,22 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'react-redux';
+import store from 'store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Auth0Provider
-        domain="vtormall.au.auth0.com"
-        clientId="tI3QcGzM3tab11NcGjhhTOocItXg57sw"
-        redirectUri={window.location.origin}>
-        <App />
-      </Auth0Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Auth0Provider
+          domain="vtormall.au.auth0.com"
+          clientId="tI3QcGzM3tab11NcGjhhTOocItXg57sw"
+          redirectUri={window.location.origin}>
+          <App />
+        </Auth0Provider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
