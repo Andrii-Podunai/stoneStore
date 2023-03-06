429bd11cd1833369a7f8d4a22c05d2535ce06163
Andrii Podunai <podunaiandriy2004@gmail.com>
2023-03-06 19:51:43 +0000

    add my info route

--- server/src/routes/my/index.js
+++ server/src/routes/my/index.js
@@ -1,5 +1,6 @@
 import CardRepositories from '../../repositories/cards.js';
 import FavoritesRepositories from '../../repositories/favorites.js';
+import InfoRepositories from '../../repositories/infoUser.js';
 import Schemas from '../../schemas/index.js';
 
 function getMyCardsHandler(request, reply) {
@@ -26,6 +27,22 @@ function removeMyFavoritesHandler(request, reply) {
     .catch((error) => reply.code(error.status || 500).send(error));
 }
 
+function getMyInfoHandler(request, reply) {
+  InfoRepositories.getMyInfo(request.user.sub)
+    .then((data) => reply.code(200).send(data))
+    .catch((error) => reply.code(error.status || 500).send(error));
+}
+
+function addMyInfoHandler(request, reply) {
+  InfoRepositories.addMyInfo(request.user.sub, request.body)
+    .then((data) => reply.code(200).send(data))
+    .catch((error) => reply.code(error.status || 500).send(error));
+}
+function removeMyInfoHandler(request, reply) {
+  InfoRepositories.removeMyInfo(request.user.sub, request.body)
+    .then((data) => reply.code(200).send(data))
+    .catch((error) => reply.code(error.status || 500).send(error));
+}
 export default (fastify, __, done) => {
   fastify.route({
     method: 'GET',
@@ -52,6 +69,23 @@ export default (fastify, __, done) => {
     preValidation: fastify.authenticate,
     handler: removeMyFavoritesHandler,
   });
-
+  fastify.route({
+    method: 'GET',
+    url: '/information',
+    preValidation: fastify.authenticate,
+    handler: getMyInfoHandler,
+  });
+  fastify.route({
+    method: 'POST',
+    url: '/information',
+    preValidation: fastify.authenticate,
+    handler: addMyInfoHandler,
+  });
+  fastify.route({
+    method: 'PATCH',
+    url: '/information',
+    preValidation: fastify.authenticate,
+    handler: removeMyInfoHandler,
+  });
   done();
 };
